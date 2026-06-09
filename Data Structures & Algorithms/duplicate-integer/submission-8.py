41f77689e497b4bb0abeec72c0efb9a4487576a0
tjenki7290 <jenkinsty219@gmail.com>
2026-06-09 10:50:55 -0400

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums):
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+        return False    
