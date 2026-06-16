2a683e0284fc2f0a8c4edf3836ff62c4464a9892
tjenki7290 <jenkinsty219@gmail.com>
2026-06-16 14:22:39 -0400

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        #build the count dict, key is integer, value is frequency
+        count = defaultdict(int)
+
+        for i in nums: 
+            count[i] += 1
+
+        #this is going to be the key,value pairs, 
+        pairs = [(count[n], n) for n in count] #I made the first element the frequency, hence the count[n]
+        pairs.sort(reverse = True) #sorts by first element, which is why i created the pairs with frequency as first element. flips the sort to largest - smallest
+
+        result = [pair[1] for pair in pairs[:k]]
+
+        return result
